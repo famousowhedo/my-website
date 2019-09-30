c42dc76180e5c9eb4d3c162d7a30a5a512dd45b6
famousowhedo <owhedofamous12345@gmail.com>
2019-09-30 12:30:31 +0000

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+setTimeout (function(){
+    document.getElementById('mac').setAttribute('src', 'famous/p.jpg')
+   
+},3000);
+
+setTimeout (function(){
+    document.getElementById('paul').setAttribute('src', 'famous/showcas.jpg')
+},6000);
+
+setTimeout (function(){
+    document.getElementById('demo').setAttribute('src','famous/n.jpg');
+
+},9000);
+
+
+
